13a68bc9e12f84b48a5806d2491b863f8cadf54c
PauloRFOliveira <paulo.oliveira.701@ufrn.edu.br>
2022-10-12 02:42:48 -0300

    Feature: Criação da navegação para a tela de visualização da carta individual e diagramação dessa mesma tala.

--- screens/CardScreen.js
+++ screens/CardScreen.js
@@ -1,11 +1,13 @@
 import React from "react";
-import {View, StyleSheet} from "react-native";
+import {View, StyleSheet, Image} from "react-native";
 
-export default function CardScreen(){
+export default function CardScreen({route}){
+    const {image} = route.params;
+
     return(
         <View style={styles.container}>
 
-
+            <Image style={styles.cardImage} source={{uri: image}}/>
 
         </View>
     );
@@ -15,4 +17,10 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    cardImage: {
+        flex: 1,
+        width: null,
+        height: null,
+        resizeMode: 'contain',
+    }
 });
